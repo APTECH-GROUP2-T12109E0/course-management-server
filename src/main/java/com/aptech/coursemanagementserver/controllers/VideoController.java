119e362938a26968780f532956a94c19a4327e71
trucduy10 <90845140+trucduy10@users.noreply.github.com>
2023-07-18 17:45:01 +0000

    fix GlobalStorage

--- src/main/java/com/aptech/coursemanagementserver/controllers/VideoController.java
+++ src/main/java/com/aptech/coursemanagementserver/controllers/VideoController.java
@@ -1,9 +1,11 @@
 package com.aptech.coursemanagementserver.controllers;
 
 import static com.aptech.coursemanagementserver.constants.GlobalStorage.CAPTION_API;
+import static com.aptech.coursemanagementserver.constants.GlobalStorage.DEV_DOMAIN_API;
 import static com.aptech.coursemanagementserver.constants.GlobalStorage.CAPTION_PATH;
 import static com.aptech.coursemanagementserver.constants.GlobalStorage.FETCHING_FAILED;
 import static com.aptech.coursemanagementserver.constants.GlobalStorage.STREAM_API;
+import static com.aptech.coursemanagementserver.constants.GlobalStorage.DEV_DOMAIN_API;
 import static com.aptech.coursemanagementserver.constants.GlobalStorage.VIDEO_PATH;
 
 import java.nio.file.Files;
@@ -86,14 +88,14 @@ public class VideoController {
                 Files.copy(captionFile.getInputStream(),
                         CAPTION_PATH.resolve(captionFile.getOriginalFilename()),
                         StandardCopyOption.REPLACE_EXISTING);
-                captionUrls.add(CAPTION_API + captionFile.getOriginalFilename());
+                captionUrls.add(DEV_DOMAIN_API + CAPTION_API + captionFile.getOriginalFilename());
             }
 
             VideoDto videoDto = new VideoDto();
             videoDto.setLessonId(lessonId);
             videoDto.setName(generateFilename(Instant.now()) + videoFile.getOriginalFilename());
             videoDto.setUrl(
-                    STREAM_API + videoExtension + "/" + videoDto.getName().split("_")[0] + "_"
+                    DEV_DOMAIN_API + STREAM_API + videoExtension + "/" + videoDto.getName().split("_")[0] + "_"
                             + FilenameUtils.getBaseName(videoFile.getOriginalFilename()));
             videoDto.setCaptionUrls(captionUrls);
 
