03fc716b309850a7906dafb9a111ef3107eaea43
NguyenNguyenTally <124548339+NguyenNguyenTally@users.noreply.github.com>
2023-07-23 07:35:33 +0700

    Fix enroll and checkout

--- src/main/java/com/aptech/coursemanagementserver/controllers/CheckoutController.java
+++ src/main/java/com/aptech/coursemanagementserver/controllers/CheckoutController.java
@@ -56,6 +56,8 @@ public class CheckoutController {
             throws Exception {
         try {
             return ResponseEntity.ok(checkoutService.checkoutPayment(checkoutDto));
+        } catch (BadRequestException e) {
+            throw new BadRequestException(e.getMessage());
         } catch (Exception e) {
             throw new BadRequestException(GLOBAL_EXCEPTION);
         }
